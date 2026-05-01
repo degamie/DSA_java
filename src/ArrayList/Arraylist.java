b59a92ea9a2d5917fbb91b786491f70731042932
Degamie_sign <degamiesign@gmail.com>
2026-05-01 09:56:25 +0530

    ArrayList#1

---
+++ src/ArrayList/Arraylist.java
@@ -0,0 +1,13 @@
+//WID(1/5/2026)(Sarthak Mittal)(DegamiesSign)#1
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist {
+    ArrayList<String> arr=new ArrayList<>();
+    arr.add("Sarthak Mittal");
+    public ArrayList<String> getArr() {
+        return arr;
+    }
+}
