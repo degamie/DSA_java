e87a9db41f43b72848310305ea79fa7cb2c8fa60
Degamie_sign <degamiesign@gmail.com>
2026-05-02 07:49:11 +0530

    LinkedlIst#1

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,8 @@
+package LinkedList;
+import  java.util.LinkedList;
+public class LinkedList {
+    public static  void Main(String[] args){
+       LinkedList<String,String> l1=new LinkedList<>();
+       l1.add("Sarthak","Mittal");
+    }
+}
