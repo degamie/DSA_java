f93b325d01a0a922fddd64d124b0f584680400e0
Degamie_sign <degamiesign@gmail.com>
2026-05-08 13:00:55 +0530

    ArrayList#1_1_java8_Impl

--- src/java8_practice/Arrays/Arrayscls.java
+++ src/java8_practice/Arrays/Arrayscls.java
@@ -1,30 +1,26 @@
-//WID(7/5/2026)(Sarthal Mittal)#1
+//WID(8/5/2026)(Sarthal Mittal)#1.1
 package java8_practice.Arrays;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-public class Arrayscls {
-    public static void main(String[] args){
-        List<List<String>> skills= Arrays.asList(
+//T(C(N))==O(1)  and S(C(N))==O(N)  as it requires contiguos space alloc in unit Time;
+public class Arrayscls {//Arrays class Declare
+    public static void main(String[] args){//main () method declare
+        List<List<String>> skills= Arrays.asList(// Sklls's arrayList elem declare
+                Arrays.asList("Python","MatPlotLib","OpenCV2","PyGame","Blender3D"),
                 Arrays.asList("Java","Spring","SpringBoot"),
                 Arrays.asList("JavaScript","AngularJS","ReactJs"),
                 Arrays.asList("Android","FireBase","FireBaseDB"),
                 Arrays.asList("SQL","MYSQL","oraclesqldev"),
                 Arrays.asList("VCS",".git","SVN","SourceTree","GitHub"),
-                Arrays.asList("Deployment","Oracle WebLogic 12C"),
-                Arrays.asList("GameDev","Unreal Engine","Unity","LiBGDX")
+                Arrays.asList("Deployment","Oracle WebLogic 12C","itch.io"),
+                Arrays.asList("GameDev","Unreal Engine","Unity","LiB(GD)X")
         );
+        //Mapping multiple SkillSet's Elements into 1 combined List's Stream and Collecting its Elements in an immutablelist
         List<String> skillset=skills.stream().flatMap(skillsList->skillsList.stream()).collect(Collectors.toUnmodifiableList());
-        System.out.println(skillset);
+        System.out.println(skillset);//pritning SkillSet
     }
 }
-//TO Be Fixed
-//        skillset.stream().filter(skillset.stream().filter(skills->skills.startsWith("J")).collect(Collectors.toList());
-//        Stream<String> GDSkillStream = skillset.stream().filter(skillset.stream().filter(skills -> skills.startsWith("G")).collect(Collectors.toList()).get(skills));
-
-//    skillsList.stream(filter(skills.allSkills.sr))
-
 
