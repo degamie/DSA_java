979708413ff6cd388b2791c0150e8ca9bdbfc284
Degamie_sign <degamiesign@gmail.com>
2026-05-14 08:01:14 +0530

    LinkedList's vars nd construct#1

--- DSA_java/src/LinkedList/LinkedLists.java
+++ DSA_java/src/LinkedList/LinkedLists.java
@@ -1,9 +1,16 @@
-//WID(7/5/2026)(Sarthak Mittal)(DegamieSign)
+//WID(14/5/2026)(Sarthak Mittal)(DegamieSign)#1
 package LinkedList;
 import  java.util.LinkedList;
 import java.util.List;
 
 public class LinkedLists {
+    E item;
+    Node<E> prev;    Node<E> next;
+    LinkedLists(E item, Node<E> prev, Node<E> next){
+        this.item=item;
+        this.prev=prev;
+        this.next=next;
+    }
     public static  void main(String[] args){
       List<String> l1=new LinkedList();
        l1.add(0,"Sarthak");
