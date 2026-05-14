52bb5a1dfac7270bb0cd90088d1f371bcced906b
Degamie_sign <degamiesign@gmail.com>
2026-05-14 18:19:49 +0530

    ConcurrentHashSet_IMpl#1

--- DSA_java/src/HashSet/HashSets.java
+++ DSA_java/src/HashSet/HashSets.java
@@ -1,36 +1,48 @@
-////WID(5/5/2026)(Sarthak Mittal)#1HashSets
-////Common Elements(T(C(N))==O(n) and S(C(N))==O(n^2 ) as it requires both dynamic and contigous space iteratively
-//package HashSet;
-//import   java.util.HashSet;
-//import java.util.Set;
-//
-//public class HashSets {
-//
-//    public static Set<Integer>getIntersection(int[] arr1,int[] arr2){
-//        Set<Integer> hashset=new HashSet<>();
-//        for(int num:arr1){
-//           hashset.add(num);
-//        }
-//        for(int num:arr2){
-//            Set<Integer> commonhashSet=new HashSet<>();
-//            if(hashset.contains(num)){
-//                commonhashSet.add(num);
-//            }
-//        }return hashset;
+//WID(14/5/2026)(Sarthak Mittal)#1HashSets.1
+//Common Elements(T(C(N))==O(n) and S(C(N))==O(n^2 ) as it requires both dynamic and contigous space iteratively
+package HashSet;
+import   java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class HashSets {
+
+    public static Set<Integer>getIntersection(int[] arr1,int[] arr2){
+        Set<Integer> hashset=new HashSet<>();
+        for(int num:arr1){
+           hashset.add(num);
+        }
+        for(int num:arr2){
+            Set<Integer> commonhashSet=new HashSet<>();
+            if(hashset.contains(num)){
+                commonhashSet.add(num);
+            }
+        }return hashset;
+    }
+        public static  void Main(String[] args){
+            int[] arr1 = new int[0];int[] arr2=new int[0];
+            getIntersection(arr1,arr2);
+        }
+
+}
+public class ConcurrentHashSet {
+    public static String userId=null;
+    public static  void main(String[] args){
+        final Set<Integer>activeUsers=new ConcurrentSkipListSet<>();
+        void trackUsers(userId){
+            activeUsers.add(userId);
+        }
+    }
+
+}
+
+
+
+//    public static void Main(String[] args){
+//        HashSet<Integer> hashSet=new HashSet<>();
+//        hashSet.put(1);
+//        hashSet.put(2);
+//        hashSet.put(3);
+//        System.out.println(hashSet);
 //    }
-//        public static  void Main(String[] args){
-//            int[] arr1 = new int[0];int[] arr2=new int[0];
-//            getIntersection(arr1,arr2);
-//        }
-//
-//
-//}
-//
-////    public static void Main(String[] args){
-////        HashSet<Integer> hashSet=new HashSet<>();
-////        hashSet.put(1);
-////        hashSet.put(2);
-////        hashSet.put(3);
-////        System.out.println(hashSet);
-////    }
-//
+
