cc4f8c50b49f3d3252fb567cbca0a494d252f92b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-06-30 11:57:16 +0530

    Add files via upload

--- DSA_java/src/Threading/Threadcls.java
+++ DSA_java/src/Threading/Threadcls.java
@@ -1,8 +1,27 @@
+//1
 package Threading;
 
 public class Threadcls extends Thread{
-    public Thread thread=new Thread();
-    public void run(){
-        System.out.println("Starting Thread !");
+    ThreadMain tm=new ThreadMain();
+    public Thread th=new Thread(tm);
+    public static void main(String[] args){
+//Printing Thread's Task 10000's Times
+            for(int i=0;i<10000;i++){
+        System.out.println("Starting Thread !"+ThreadMain.class);
+
+    }
+
     }
 }
+//Executing ThreadMain's run() method
+
+//            tm.run();
+
+
+
+//    Checking Task execution infinitely
+//        for(;;){
+//            System.out.println("Starting Thread !");
+//        }
+
+
