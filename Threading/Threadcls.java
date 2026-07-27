97e15dc0dba14d5371c5ffff89483102438c9e64
Degamie_sign <degamiesign@gmail.com>
2026-07-27 18:25:15 +0530

    ExecutorService_threading

--- Threading/Threadcls.java
+++ Threading/Threadcls.java
@@ -1,17 +1,18 @@
 //1
 package Threading;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Threadcls extends Thread{
-    ThreadMain tm=new ThreadMain();
-    public Thread th=new Thread
-
-
-
-        (tm);
+    Threading.ThreadMain tm=new Threading.ThreadMain();
+    public Thread th=new Thread(tm);
+    public ExecutorService executorService=new Executors.newFixedThreadPool(3); 
+    }
     public static void main(String[] args){
 //Printing Thread's Task 10000's Times
             for(int i=0;i<10000;i++){
-        System.out.println("Starting Thread !"+ThreadMain.class);
+        System.out.println("Starting Thread !"+ Threading.ThreadMain.class);
 
     }
 
